ccd0c14b9d5c7a3646ff7ee48b0027f2e2edae39
Pablogd <dominguezpg.99@gmail.com>
2025-06-16 11:00:49 -0300

    Diseño Nav

--- Frontend/pedidos-ya/src/components/Header.jsx
+++ Frontend/pedidos-ya/src/components/Header.jsx
@@ -1,11 +1,44 @@
-
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
+import Navbar from "react-bootstrap/Navbar";
+import NavDropdown from "react-bootstrap/NavDropdown";
+import Logo from "../img/logoPedidosYa.png";
+import { IoPersonOutline } from "react-icons/io5";
+import { IoIosSearch } from "react-icons/io";
+import '../css/header.css'
 
 const Header = () => {
   return (
-    <div>
-      Header
-    </div>
-  )
-}
+    <Navbar expand="lg" className="row navbar-header">
+      <div className="col">
+        <img src={Logo} className="img-logo"/>
+      </div>
+      <div className="col-6">
+        <Form className="d-flex search-container">
+          <Form.Control
+            type="Search"
+            placeholder="Buscar Locales"
+            className="me-2 input-header"
+            aria-label="Search"
+          />
+          <button className="boton-buscar-header">
+            <IoIosSearch className="boton-buscar-header-i"/>
+          </button>
+        </Form>
+      </div>
+      <div className="col d-flex  justify-content-center">
+        <div>
+          <IoPersonOutline />
+        </div>
+        <div>
+          <NavDropdown title="Mi Perfil" id="navbarScrollingDropdown"> 
+          <NavDropdown.Item href="#action3">Ayuda en Linea</NavDropdown.Item>
+          <NavDropdown.Item href="#action4">Iniciar Sesion</NavDropdown.Item>
+        </NavDropdown>
+        </div>
+      </div>
+    </Navbar>
+  );
+};
 
-export default Header
+export default Header;
