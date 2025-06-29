49f06f96a97ef674633bbaf036138e90a2e1069c
GiulianoArk <calcagnigiuliano770@gmail.com>
2025-06-29 19:37:13 -0300

    se mejoró el css del carrito

--- Frontend/src/components/MainCarrito.jsx
+++ Frontend/src/components/MainCarrito.jsx
@@ -1,13 +1,13 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
 import useUsuarioStore from "../store/usuarioStore";
+import "../css/maincarrito.css"; 
 
 const MainCarrito = () => {
   const [aclaracion, setAclaracion] = useState("");
   const [metodoPago, setMetodoPago] = useState("efectivo");
   const [carrito, setCarrito] = useState([]);
   const idUsuario = useUsuarioStore((state) => state.usuario?.idUsuario);
-  console.log("ID Usuario:", idUsuario);
 
   useEffect(() => {
     const fetchCarrito = async () => {
@@ -32,35 +32,44 @@ const MainCarrito = () => {
   };
 
   return (
-    <div>
-      <h2>Confirmar compra</h2>
-      <form onSubmit={handleSubmit}>
-        <input
-          type="text"
-          placeholder="Aclaraciones"
-          value={aclaracion}
-          onChange={(e) => setAclaracion(e.target.value)}
-        />
-        <select
-          value={metodoPago}
-          onChange={(e) => setMetodoPago(e.target.value)}
-        >
-          <option value="">Método de pago</option>
-          <option value="Efectivo">Efectivo</option>
-          <option value="Tarjeta">Tarjeta</option>
-        </select>
-        <button type="submit">Confirmar compra</button>
-      </form>
+    <div className="main-carrito">
+      <div className="cardProductos">
+        <h2>Mi pedido</h2>
+        {carrito.length === 0 ? (
+          <p>No hay productos en el carrito.</p>
+        ) : (
+          carrito.map((p) => (
+            <div key={p.id} className="producto-item">
+              <span>{p.nombre}</span>
+              <span>
+                {p.cantidad} x ${p.precio_unitario}
+              </span>
+            </div>
+          ))
+        )}
+      </div>
 
-      <h3>Carrito</h3>
-      {carrito.map((p) => (
-        <div key={p.id}>
-          <span>{p.nombre}</span> —{" "}
-          <span>
-            {p.cantidad} x ${p.precio_unitario}
-          </span>
-        </div>
-      ))}
+      <div className="cardResumen">
+        <h2>Resumen</h2>
+        <form onSubmit={handleSubmit}>
+          <input
+            type="text"
+            placeholder="Aclaraciones"
+            value={aclaracion}
+            onChange={(e) => setAclaracion(e.target.value)}
+          />
+          <select
+            value={metodoPago}
+            onChange={(e) => setMetodoPago(e.target.value)}
+          >
+            <option value="">Método de pago</option>
+            <option value="Efectivo">Efectivo</option>
+            <option value="Tarjeta">Tarjeta</option>
+          </select>
+          <button type="submit">Pasar a finalizar compra</button>
+        </form>
+        <div className="total"></div>
+      </div>
     </div>
   );
 };
